27e80e87adaf1f9cb779f715b97366470979bc2a
Azizz09 <tasciaziz163@gmail.com>
2026-04-14 23:09:45 +0300

    Update script.js

--- script.js
+++ script.js
@@ -131,7 +131,7 @@ actionBtn.addEventListener('click', () => {
                 setTimeout(() => copyBtn.innerText = "Copy Text", 2000);
             };
         }
-        actionBtn.innerText = "ENLIGHTEN ME";
+        actionBtn.innerText = "GIVE ME A FACT";
         actionBtn.disabled = false;
     }, 400);
 });
